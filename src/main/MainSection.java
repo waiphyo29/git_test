633237a3477aa100d8196ce671ef42c508cc5713
WaiPhyoAung <ww.phyo29aung@gmail.com>
2023-09-25 18:20:31 +0630

    updated added gitIgnore File and modified MainSection.java changed editor

--- src/main/MainSection.java
+++ src/main/MainSection.java
@@ -5,6 +5,8 @@ public class MainSection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inside Main Method ...");
+		System.out.println("Inside Main Method ...");
 	}
 
 }
+
